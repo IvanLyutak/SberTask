a56154559d7005219dcc3990cd8a340a2fcb33f8
Иван Лютак <ivanlyutak@Ivans-MacBook-Pro-2.local>
2023-08-08 13:53:14 +0300

    add date validator

--- schemas/deposits.py
+++ schemas/deposits.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, field_validator
+from datetime import datetime
 
 class DepositSchema(BaseModel):
     date: str
     periods: int = Field(ge=1, le=60)
     amount: int = Field(ge=10000, le=3000000)
     rate: float = Field(ge=1.0, le=8.0)
+
+    @field_validator('date')
+    def parse_date(cls, date):
+        return datetime.strptime(date, "%d.%m.%Y").date()
